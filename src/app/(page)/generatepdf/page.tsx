d889ea22a64e9ecf6c6457756760da3533b69a6f
Cons Golosino <consorcio.golosino@avensia.com>
2025-05-17 14:21:04 +0800

    fix build error

--- src/app/(page)/generatepdf/page.tsx
+++ src/app/(page)/generatepdf/page.tsx
@@ -1,5 +1,7 @@
 import GeneratePdfClient from './components/pdf-genrator/generatepdfclient';
 
+export const dynamic = 'force-dynamic'; // Ensure server doesn't try to prerender
+
 const GeneratePDF = () => {
   return <GeneratePdfClient />;
 };
